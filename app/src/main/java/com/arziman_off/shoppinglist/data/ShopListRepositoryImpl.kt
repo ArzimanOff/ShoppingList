73ec1a568302928e4c8ec4899be00de6dcfb006a
ArzimanOff <arzimanovgyulahmed@yandex.ru>
2024-09-29 18:48:02 +0300

    data слой

---
+++ app/src/main/java/com/arziman_off/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.arziman_off.shoppinglist.data
+
+import com.arziman_off.shoppinglist.domain.ShopItem
+import com.arziman_off.shoppinglist.domain.ShopListRepository
+
+class ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldItem: ShopItem = getShopItem(shopItem.id)
+        deleteShopItem(oldItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Элемент с id $shopItemId не найден")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
