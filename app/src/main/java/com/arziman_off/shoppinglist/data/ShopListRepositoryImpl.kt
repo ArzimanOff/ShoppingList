5fbef6f8be415a65abe9e939bcf7101948bec0a8
ArzimanOff <arzimanovgyulahmed@yandex.ru>
2024-09-30 23:24:24 +0300

    реализовал основу адаптера для RecyclerView

--- app/src/main/java/com/arziman_off/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/arziman_off/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,19 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.arziman_off.shoppinglist.domain.ShopItem
 import com.arziman_off.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
 
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({
+        o1, o2 -> o1.id.compareTo(o2.id)
+    })
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10) {
+        for (i in 0 until 100) {
             val item = ShopItem(
                 "Name #$i",
                 i,
-                true
+                Random.nextBoolean()
             )
             addShopItem(item)
         }
