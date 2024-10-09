b9dc835b61dd290ee81aa55b542199d93bac6611
ArzimanOff <arzimanovgyulahmed@yandex.ru>
2024-10-09 23:55:33 +0300

    начал переход на фрагменты

---
+++ app/src/main/java/com/arziman_off/shoppinglist/presentation/ShopItemFragment.kt
@@ -0,0 +1,22 @@
+package com.arziman_off.shoppinglist.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.arziman_off.shoppinglist.R
+
+class ShopItemFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_shop_item,
+            container,
+            false
+        )
+    }
+}
